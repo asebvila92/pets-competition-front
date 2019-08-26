d576496203454d57d26fc160bef3efd27f4d04ec
Andres <libra29_92@hotmail.com>
2019-08-26 03:16:05 -0300

    change axios get of petsApi

--- src/api/petsApi.js
+++ src/api/petsApi.js
@@ -1,21 +1,21 @@
 import axios from 'axios';
 
-export const getPetsOfPerson = (person) => {
-  return axios.get("http://localhost:4881/api/pets");
-}
+export const getPetsOfPerson = personId => {
+  return axios.get('http://localhost:4881/api/pets/' + personId);
+};
 
-export const postPet = (pet) => {
-  return new Promise((resolve) => {
-    setTimeout(()=>{
-      resolve({data: "OK PostPet"})
-    })
-  }, 6000)
-}
+export const postPet = pet => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve({ data: 'OK PostPet' });
+    });
+  }, 6000);
+};
 
-export const deletePet = (pet) => {
-  return new Promise((resolve) => {
-    setTimeout(()=>{
-      resolve({data: "OK DeletePet"})
-    })
-  }, 6000)
-}
+export const deletePet = pet => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve({ data: 'OK DeletePet' });
+    });
+  }, 6000);
+};
