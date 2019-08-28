d4a8cfc430ae7094e317ca21eca699b175a50b7d
Andres <libra29_92@hotmail.com>
2019-08-28 02:52:19 -0300

    Change double commas to simple commas

--- src/redux/reducers/reducerPet.js
+++ src/redux/reducers/reducerPet.js
@@ -1,28 +1,28 @@
-const reducerPet = (state = {listPetsOfPerson: [], person:{} }, action) => {
-  switch(action.type){
-    case "GET_PETS_PERSON_SUCCESS": {
-      return {...state, listPetsOfPerson: action.payload}
+const reducerPet = (state = { listPetsOfPerson: [], person: {} }, action) => {
+  switch (action.type) {
+    case 'GET_PETS_PERSON_SUCCESS': {
+      return { ...state, listPetsOfPerson: action.payload };
     }
-    case "GET_PETS_PERSON_ERROR": {
-      return {...state, error: "error de GET"}
+    case 'GET_PETS_PERSON_ERROR': {
+      return { ...state, error: 'error de GET' };
     }
-    case "POST_PET_SUCCESS": {
+    case 'POST_PET_SUCCESS': {
+      const newlist = action.payload;
+      return { ...state, listPetsOfPerson: newlist };
+    }
+    case 'POST_PET_ERROR': {
+      return { ...state, error: 'error de POST' };
+    }
+    case 'DELETE_PET_SUCCESS': {
       const newlist = [...state.listPetsOfPerson, action.payload];
-      return {...state, listPetsOfPerson: newlist}
+      return { ...state, listPetsOfPerson: newlist };
     }
-    case "POST_PET_ERROR": {
-      return {...state, error: "error de POST"}
-    }
-    case "DELETE_PET_SUCCESS": {
-      const newlist = [...state.listPetsOfPerson, action.payload];
-      return {...state, listPetsOfPerson: newlist}
-    }
-    case "DELETE_PET_ERROR": {
-      return {...state, error: "error de DELETE"}
+    case 'DELETE_PET_ERROR': {
+      return { ...state, error: 'error de DELETE' };
     }
     default:
       return state;
   }
-}
+};
 
 export default reducerPet;
